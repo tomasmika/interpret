283bb041efacfcd10aa5a8c1054c469100ce1ca0
Tomas Bosschieter <76884339+tomasmika@users.noreply.github.com>
2024-01-18 14:50:49 -0800

    Create setup.py
    Signed-off-by: Tomas Bosschieter <76884339+tomasmika@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='interpret',
+    version='0.5.0',  # You can adjust the version number as needed
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
